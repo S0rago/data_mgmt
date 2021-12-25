ca5897d36ce3787e26a30838ba669de9c4be8f57
Sorago <linehalt10909@yandex.ru>
2021-12-25 23:53:00 +0300

    minor fixes(a whole lot)

--- image-resizer/db.py
+++ image-resizer/db.py
@@ -1,26 +1,44 @@
 import typing
-from numpy import ndarray
+
+
+class ImageFile:
+    def __init__(self):
+        self.id: int = 0
+        self.name: str = ""
+        self.content: bytes = None
+
+    def __init__(self, id, name, content):
+        self.id: int = id
+        self.name: str = name
+        self.content: bytes = content
+    
 
 
 class Database:
     def __init__(self):
-        self._items: typing.Dict[int, ndarray] = {}
+        self._items: typing.Dict[int, ImageFile] = {}
         self._seq: int = 0
 
-    def get(self, id: int) -> ndarray:
+    def getbyid(self, id: int) -> ImageFile:
         image = self._items.get(id)
         if image is not None:
             return image
         else:
-            raise ValueError("Image not found with id =", id)
+            raise ValueError(f"Image not found with id = {id}")
 
-    def add(self, image: ndarray) -> int:
+    def getbyname(self, name: str) -> ImageFile:
+        for id, image in self._items.items():
+            if image.name == name:
+                return image
+        else:
+            raise ValueError(f"Image not found with name = {name}")
+        
+
+    def add(self, filename: str, image: bytes) -> int:
         self._seq += 1
-        self._items[self._seq] = image
+        self._items[self._seq] = ImageFile(self._seq, filename, image)
         return self._seq
 
     def delete(self, id: int):
-        if id in self._items.keys:
-            del self._items[id]
-        else:
-            raise ValueError("Image not found with id =", id)
+        if self._items.pop(id, None):
+            raise ValueError(f"Image not found with id = {id}")
--- image-resizer/main.py
+++ image-resizer/main.py
@@ -4,23 +4,52 @@ from io import BytesIO
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi.datastructures import UploadFile
-from fastapi.responses import Response
-from fastapi.params import File
+from fastapi.params import File, Form
 from starlette.responses import StreamingResponse
 from db import Database
 from cv2 import imdecode, resize, IMREAD_COLOR, imencode
 import numpy as np
 
-
 app = FastAPI(title="Image resizer")
 db = Database()
+root_api = os.getenv("BASE_API", '/images')
 
 
-@app.get("/get/{id}")
-async def get(id: int, mult: float):
+@app.get(root_api + "/getbyid/{id}")
+async def getbyid(id: int):
     try:
-        nparr = db.get(id)
-        img = imdecode(nparr, IMREAD_COLOR)
+        file = db.getbyid(id)
+        image = file.content
+    except Exception as e:
+        raise HTTPException(404, str(e))
+    return StreamingResponse(content=BytesIO(image), media_type="image/png")
+
+
+@app.get(root_api + "/getbyname/{name}")
+async def getbyname(name: str):
+    try:
+        file = db.getbyname(name)
+        image = file.content
+    except Exception as e:
+        raise HTTPException(404, str(e))
+    return StreamingResponse(content=BytesIO(image), media_type="image/png")
+
+
+@app.get(root_api + "/getsize/{id}")
+async def getsize(id: int):
+    try:
+        file = db.getbyid(id)
+        image = file.content
+    except Exception as e:
+        raise HTTPException(404, str(e))
+    return {"file_size": len(image)}
+
+
+@app.get(root_api + "/modify/{id}")
+async def modify(id: int, mult: float):
+    try:
+        file = db.getbyid(id)
+        img = imdecode(np.frombuffer(file.content, dtype=np.uint8), IMREAD_COLOR)
         dsize = (int(img.shape[0] * mult), int(img.shape[1] * mult))
         resized = resize(img, dsize)
         res, enc_img = imencode(".jpeg", resized)
@@ -29,24 +58,25 @@ async def get(id: int, mult: float):
     return StreamingResponse(content=BytesIO(enc_img.tobytes()), media_type="image/png")
 
 
-@app.post("/add")
-async def add(image: UploadFile = File(...)):
+@app.post(root_api + "/add")
+async def add(filename: str = Form(...), image: UploadFile = File(...)):
     try:
         content = await image.read()
-        nparr = np.fromstring(content, np.uint8)
-        id = db.add(nparr)
+        id = db.add(filename, content)
     except Exception as e:
         raise HTTPException(404, str(e))
     return {"id": id}
 
 
-@app.delete("/delete/{id}")
+@app.delete(root_api + "/delete/{id}")
 async def delete(id: int):
     try:
         db.delete(id)
     except Exception as e:
         raise HTTPException(404, str(e))
+    return {"deleted": id}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=os.getenv('HOST'), port=os.getenv('PORT'))
+    uvicorn.run(app, host=os.getenv('HOST', 'localhost'),
+                port=int(os.getenv('PORT', '6969')))
